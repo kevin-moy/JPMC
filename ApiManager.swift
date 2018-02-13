baea2054bec0c85de0f1c5175f335af72d738724
kevin <kevintmoy@gmail.com>
2018-02-13 13:52:40 -0800

    add apimanager and vc

---
+++ ApiManager.swift
@@ -0,0 +1,69 @@
+//
+//  ApiManager.swift
+//  JPMC
+//
+//  Created by Kevin on 2/12/18.
+//  Copyright © 2018 Kevin. All rights reserved.
+//
+
+import Foundation
+
+class ApiManager {
+    
+    static let sharedInstance = ApiManager()
+    
+    func getUpcomingLaunches(_ launchFilter: String?, startDateFilter: String?, endDateFilter: String?, completionHandler:@escaping (_ launches: [LaunchObject]?, _ error: String?) -> Void) {
+        
+        var launchArray = [LaunchObject]()
+        var urlString = Constants.spacexURL
+        // https://api.spacexdata.com/v2/launches/upcoming?launch_year=2018&start=2018-02-18&final=2018-03-19&
+        if launchFilter != nil {
+            urlString.append(Constants.launchYearURL + launchFilter!)
+        }
+        if startDateFilter != nil {
+            urlString.append(Constants.startDateURL + startDateFilter!)
+        }
+        
+        if endDateFilter != nil {
+            urlString.append(Constants.endDateURL + endDateFilter!)
+        }
+        
+        let url = URL(string: urlString)!
+        let group = DispatchGroup()
+        group.enter()
+        let task = URLSession.shared.dataTask(with: url, completionHandler: {data, response, error -> Void in
+            
+            guard error == nil else {
+                print(error!)
+                completionHandler(nil, error as! String?)
+                return
+            }
+            guard data != nil else {
+                print("Data is empty")
+                return
+            }
+            
+            if error != nil {
+                print("Error fetching data: \(String(describing: error))")
+                return
+            }
+            
+            do {
+                let json = try! JSONSerialization.jsonObject(with: data!, options: [])
+                guard let spacexJSON = json as? [[String: Any]] else {
+                    completionHandler(nil, "JSON not a dictionary")
+                    return }
+                
+                for feedData in spacexJSON {
+                    let feed = LaunchObject.init(json: feedData)
+                    launchArray.append(feed)
+                }
+                completionHandler(launchArray, nil)
+            }
+        })
+        task.resume()
+        group.notify(queue: .main) {
+            completionHandler(launchArray, nil)
+        }
+}
+}
---
+++ View/ViewController.swift
@@ -0,0 +1,123 @@
+//
+//  ViewController.swift
+//  JPMC
+//
+//  Created by Kevin on 2/12/18.
+//  Copyright © 2018 Kevin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController,  UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var filterView: UIView!
+    @IBOutlet weak var mainNavigationBar: UINavigationItem!
+    @IBOutlet weak var launchYearTextField: UITextField!
+    
+    var launchFeed = LaunchViewModel()
+    var startDateFilter: String?
+    var endDateFilter: String?
+    var launchYearFilter: String?
+    
+    // Using MVVM model
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        getLaunches()
+    }
+    
+    func getLaunches() {
+        // Get Filter
+        launchFeed.feedRequest(launchYearFilter, startDateFilter: startDateFilter, endDateFilter: endDateFilter) { (success, error) in
+            if success {
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                    if self.launchFeed.launch.count == 0 {
+                        self.noResultsAlert()
+                    }
+                }
+            }
+            else if error != nil {
+                self.noConnectionAlert()
+            }
+        }
+    }
+    func noResultsAlert() {
+        let alertController = UIAlertController.init(title: "No Results", message: "", preferredStyle: .alert)
+        let okayAction = UIAlertAction.init(title: "OK", style: .cancel, handler: nil)
+        alertController.addAction(okayAction)
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    func noConnectionAlert() {
+        let alertController = UIAlertController.init(title: "Error", message: "No Internet connection. Please try again", preferredStyle: .alert)
+        let okayAction = UIAlertAction.init(title: "OK", style: .cancel, handler: nil)
+        alertController.addAction(okayAction)
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    // MARK: - Table view data source
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! LaunchCell
+        cell.setupCell(launch: self.launchFeed.launch[indexPath.row])
+    
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return launchFeed.launch.count
+    }
+    
+    // MARK: - IB Actions
+    @IBAction func filterButtonPressed(_ sender: UIBarButtonItem) {
+        self.navigationController?.setNavigationBarHidden(true, animated: true)
+        filterView.alpha = 1
+    }
+    
+    @IBAction func cancelBarButtonPressed(_ sender: UIBarButtonItem) {
+        filterView.alpha = 0
+        self.navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+    
+    @IBAction func applyButtonPressed(_ sender: UIBarButtonItem) {
+        launchFeed.clearFeed()
+        getLaunches()
+        filterView.alpha = 0
+        launchYearTextField.resignFirstResponder()
+        self.navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+    
+    @IBAction func startDateChanged(_ sender: UIDatePicker) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        startDateFilter = dateFormatter.string(from: sender.date)
+    }
+    
+    @IBAction func finalDateChanged(_ sender: UIDatePicker) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        endDateFilter = dateFormatter.string(from: sender.date)
+    }
+    
+    // TextField Functions
+    @IBAction func launchYearFilterChanged(_ sender: UITextField) {
+          launchYearFilter = sender.text
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    // Only accept numbers
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if textField == launchYearTextField {
+            let allowedCharacters = CharacterSet.decimalDigits
+            let characterSet = CharacterSet(charactersIn: string)
+            return allowedCharacters.isSuperset(of: characterSet)
+        }
+        return true
+    }
+}
+
